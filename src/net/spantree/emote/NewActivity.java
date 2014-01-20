ece0d04a0b03fc86e0929a319175899fe05a0664
Malynda Chizek Frouard <malynda@spantree.net>
2014-01-20 11:40:36 -0600

    Added Startup Screen and started New and Old activity pages

---
+++ src/net/spantree/emote/NewActivity.java
@@ -0,0 +1,13 @@
+package net.spantree.emote;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by malynda on 1/20/14.
+ */
+public class NewActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
