d6d9d1213cf0ef1459eda7d8a0e7d983d43b9412
alliecurry <allie@spantree.net>
2014-01-20 16:01:00 -0600

    Basic button setup for worksheet activity

--- src/net/spantree/emote/NewWorksheetActivity.java
+++ src/net/spantree/emote/NewWorksheetActivity.java
@@ -2,8 +2,13 @@ package net.spantree.emote;
 
 import android.app.Activity;
 import android.os.Bundle;
+<<<<<<< HEAD
 import net.spantree.emote.util.FragmentUtil;
+=======
+import android.view.View;
+>>>>>>> Basic button setup for worksheet activity
 import android.widget.TextView;
+import net.spantree.emote.util.FragmentUtil;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -12,12 +17,27 @@ import java.util.Date;
 /**
  * Created by malynda on 1/20/14.
  */
-public class NewWorksheetActivity extends Activity {
+public class NewWorksheetActivity extends Activity implements View.OnClickListener {
+
+    private View buttonPrevious;
+    private View buttonDone;
+    private View buttonNext;
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.newemote);
         FragmentUtil.replaceFragment(this, new QuestionListFragment(), R.id.fragment);
+        setupViews();
+    }
+
+    private void setupViews() {
         showDate();
+        buttonPrevious = findViewById(R.id.worksheet_back_button);
+        buttonDone = findViewById(R.id.worksheet_done_button);
+        buttonNext = findViewById(R.id.worksheet_next_button);
+        buttonPrevious.setOnClickListener(this);
+        buttonDone.setOnClickListener(this);
+        buttonNext.setOnClickListener(this);
     }
 
     private void showDate() {
@@ -28,4 +48,32 @@ public class NewWorksheetActivity extends Activity {
         dateView.setText(dateText);
     }
 
+    /** Called when a navigation button is pressed. */
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.worksheet_back_button:
+                onBackPressed();
+                break;
+            case R.id.worksheet_next_button:
+                // TODO add next fragment to stack
+                break;
+            case R.id.worksheet_done_button:
+                // TODO save worksheet
+                finish();
+                break;
+        }
+    }
+
+    /** Called when the hardware back button is pressed. */
+    @Override
+    public void onBackPressed() {
+        if (getFragmentManager().getBackStackEntryCount() > 1) {
+            FragmentUtil.popFragmentStack(this);
+        } else {
+            // TODO show dialog confirming deletion of entry
+            // closing Activity for now...
+            finish();
+        }
+    }
 }
