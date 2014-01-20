e57a554d3b413335a27e6840abf5d3c46886a3aa
alliecurry <allie@spantree.net>
2014-01-20 15:41:34 -0600

    Date now set on Worksheet activity

--- src/net/spantree/emote/NewWorksheetActivity.java
+++ src/net/spantree/emote/NewWorksheetActivity.java
@@ -3,6 +3,11 @@ package net.spantree.emote;
 import android.app.Activity;
 import android.os.Bundle;
 import net.spantree.emote.util.FragmentUtil;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by malynda on 1/20/14.
@@ -12,5 +17,15 @@ public class NewWorksheetActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.newemote);
         FragmentUtil.replaceFragment(this, new QuestionListFragment(), R.id.fragment);
+        showDate();
     }
+
+    private void showDate() {
+        TextView dateView = (TextView) findViewById(R.id.worksheet_date);
+        String dateFormat = getString(R.string.default_date_format);
+        DateFormat formatter = new SimpleDateFormat(dateFormat);
+        String dateText = formatter.format(new Date()).toString();
+        dateView.setText(dateText);
+    }
+
 }
